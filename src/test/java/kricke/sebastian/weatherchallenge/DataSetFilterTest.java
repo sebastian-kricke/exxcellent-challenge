515abd0784fb41eafd84228f5c2cfb3212771776
Sebastian Kricke <Sebastian.Kricke@outlook.de>
2019-11-12 15:51:10 +0100

    Implemented tests for basic program sequence.

---
+++ src/test/java/kricke/sebastian/weatherchallenge/DataSetFilterTest.java
@@ -0,0 +1,72 @@
+package kricke.sebastian.weatherchallenge;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/**
+ * Tests the possible run options of DataSetFilter.
+ * 
+ * @author Sebastian Kricke <Sebastian.Kricke@outlook.de>
+ */
+public class DataSetFilterTest {
+
+	@BeforeEach
+	public void setUp() {
+	}
+
+	@Test
+	public void runDataSetFilterWithoutParameters() {
+		String expectedOutput = "\n" + 
+				"Welcome to the filter application! In next steps you can choose your data source to display the filtered result: \n" + 
+				"\n" + 
+				"(1) - Filter weather data from CSV-Source by lowest temperature difference (Difference between Min-and MaxTemperature of the day)\n" + 
+				"\n" + 
+				"(Q) - Quit \n" + 
+				"\n" + 
+				"\n" + 
+				"Selection: ";
+		final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+		System.setOut(new PrintStream(outContent));
+		System.setIn(new ByteArrayInputStream("q".getBytes()));
+
+		DataSetFilter.main();
+
+		assertTrue(outContent.toString().contains("Welcome to the filter application!"), "Expected menu didn´t started.");
+		assertTrue(outContent.toString().contains("(1)"), "Expected menu didn´t started.");
+		assertTrue(outContent.toString().contains("Selection:"), "Expected menu didn´t started.");		
+	}
+
+	@Test
+	public void runDataSetFilterWithInvalidParameters() {
+		String expectedOutput = "The call of DataSetFilter has been executed with wrong parameters.\n\n"
+				+ "Valid parameters are: \n" + "... \"--weather\", \"[filename of data source]\" n \n"
+				+ "The filename is optional. If not given as parameter the default resource will be filtered."
+				+ "or start DataSetFilter without any parameters to run menu controlled interface.";
+		final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+		System.setOut(new PrintStream(outContent));
+
+		DataSetFilter.main("--wether");
+
+		assertTrue(outContent.toString().contains(expectedOutput), "Expected help info was not displayed.");
+	}
+
+	@Test
+	public void runWeather() {
+		String expectedOutput = "Day with smallest temperature spread : 20";
+		final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+		System.setOut(new PrintStream(outContent));
+		DataSetFilter.main("--weather");
+
+		assertTrue(outContent.toString().contains(expectedOutput), "Weather calculation doesn´t returned the expected value.");
+	}
+
+}
