b95dc1528e0b06b2c3d552e62fdd19e5e994c469
Sebastian Kricke <Sebastian.Kricke@outlook.de>
2019-11-14 17:00:29 +0100

    Added tests for deserializing weather.csv

---
+++ src/test/java/kricke/sebastian/reader/CsvWeatherReaderTest.java
@@ -0,0 +1,51 @@
+package kricke.sebastian.reader;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import kricke.sebastian.weather.Weather;
+
+/**
+ * Proofs if the reading functionality for weather model works as expected.
+ * @author Sebastian Kricke <Sebastian.Kricke@outlook.de>
+ *
+ */
+class CsvWeatherReaderTest {
+
+	private static CsvWeatherReader reader;
+	
+	/**
+	 * Initializes the reader member.
+	 * 
+	 * @throws java.lang.Exception Thrown if preparation of reader initialization failed.
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		reader = new CsvWeatherReader();
+	}
+	
+	/**
+	 * Checks if the read data is matching the values.
+	 * Executed by first object from weather.csv of resources file.
+	 * @throws FoundException Should not be thrown.
+	 */
+	@Test
+	void testDeserializationWeatherFromCSV() throws Exception {	
+		List<Weather> existingWeatherData = reader.Read(null, "weather.csv");		
+		Weather expectedFirst = new Weather(1, 88, 59);
+		expectedFirst.setAverageTemperature(74);
+
+		assertEquals(expectedFirst.getAverageTemperature(), existingWeatherData.get(0).getAverageTemperature());
+		assertEquals(expectedFirst.getDayOfMonth(), existingWeatherData.get(0).getDayOfMonth());
+		assertEquals(expectedFirst.getDifference(), existingWeatherData.get(0).getDifference());
+		assertEquals(expectedFirst.getMaximumTemperature(), existingWeatherData.get(0).getMaximumTemperature());
+		assertEquals(expectedFirst.getMinimumTemperature(), existingWeatherData.get(0).getMinimumTemperature());
+	}
+
+}
